8ee66dd3cac3769fedfaeef388bbbb9825da317d
Valentin Nikolov <nikolov.v.95@gmail.com>
2015-05-19 17:10:15 +0300

    * Landing page

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,28 @@
-var app = angular.module("SocialNetwork", [
-	  "app.controllers"
-	]);
+(function() {
+    var app = angular.module("SocialNetwork", [
+        "app.controllers",
+        "app.services",
+        "ngRoute"
+    ]);
+
+    app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api');
+
+    app.config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '../partials/home.html',
+            controller: 'HomeController'
+        }).when('/FriendRequests', {
+            templateUrl: 'views/friendRequests.html',
+            controller: 'friendRequestsController'
+        }).when('/Search/:id', {
+            templateUrl: 'views/searchResults.html',
+            controller: 'searchResultController'
+        }).when('/EditProfile', {
+            templateUrl: 'views/editProfile.html',
+            controller: 'editProfileController'
+        }).when('/ChangePassword', {
+            templateUrl: 'views/changePassword.html',
+            controller: 'changePasswordController'
+        }).otherwise({ redirectTo: '/' });
+    });
+})();
--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -2,9 +2,34 @@
 	"use strict"
 	var app = angular.module("app.controllers", []);
 
-	app.controller("HomeController", function($scope, UsersFactory, UtilsFactory) {
-		$scope.title = "Kiro";
-	});
+	app.controller("HomeController", function($scope, UsersFactory, UtilsFactory, ProfileFactory) {
+        if (UtilsFactory.isLogged()) {
+            $scope.menu = 'partials/menu.html';
 
+            ProfileFactory.get(function (data) {
+                $scope.fullName = data.name;
+            }, function (data) {
+                console.log(data);
+            });
+            return;
+        }
+
+        $scope.showTitle = true;
+        $scope.loginForm = 'partials/loginForm.html';
+        $scope.registerForm = '';
+        $scope.loginLink = true;
+
+
+
+        $scope.showLogin = function () {
+            $scope.loginForm = 'partials/loginForm.html';
+            $scope.registerForm = '';
+        };
+
+        $scope.showRegister = function () {
+            $scope.registerForm = 'partials/registerForm.html';
+            $scope.loginForm = '';
+        };
+	});
 })();
 
